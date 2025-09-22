4bc907f5a623e3ad7f4d994010704300b7f107ac
Katleho Myeza <katleho.myeza@bbd.co.za>
2025-09-22 09:40:54 +0200

    added emailing service

--- script.js
+++ script.js
@@ -3,18 +3,17 @@ const noBtn = document.getElementById('noBtn');
 const noVideo = document.getElementById('noVideo');
 
 yesBtn.addEventListener('click', () => {
-  emailjs.send("service_eacfk6n", "template_94rj6lj", {
+  emailjs.send(process.env.SERVICE_ID, process.env.EMAIL_TEMPLATE_ID, {
+    email: "katlehomyeza15@gmail.com",
     to_name: "Katleho Myeza",
     from_name: "Proposal Site",
     message: "She said YES! 🎉"
   })
   .then(() => {
-    // after email send, redirect to collage page
-    window.location.href = 'collage.html';
+    window.location.href = './her-page/yena.html';
   })
   .catch((err) => {
     console.error("Email error:", err);
-    window.location.href = './her-page/collage.html';
   });
 });
 
